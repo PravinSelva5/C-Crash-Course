a5ae84e807c0738e054ab4ef540352f81e37d55e
PravinSelva5 <pravin.selvarajah.eng@gmail.com>
2020-11-24 23:44:01 -0500

    added files for LinkedIn: C++ Essential

---
+++ Cpp_Essential/Exercise Files/Chap02/hello.cpp
@@ -0,0 +1,16 @@
+// hello.cpp by Bill Weinman <http://bw.org/> COMMENTS
+/*
+ MULTI LINE COMMENTS
+ 
+ */
+
+#include <cstdio> // directive for the pre-processor
+using namespace std;
+
+// BLANK SPACES ARE IGNORED IN C++
+
+int main()
+{
+    puts("Hello, World!"); // puts() puts a string to the command line when executed
+    return 0;
+}
